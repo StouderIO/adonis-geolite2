34d61d13ed8d9e80cf9553aca1bfc3ba7690c82f
Xavier Stouder <xavier@stouder.io>
2024-05-09 14:21:22 +0200

    fix: register provider

--- configure.ts
+++ configure.ts
@@ -5,4 +5,9 @@ export async function configure(command: ConfigureCommand) {
   const codemods = await command.createCodemods()
 
   await codemods.makeUsingStub(stubsRoot, 'config.stub', {})
+
+  // add provider and directory to rc file
+  await codemods.updateRcFile((transformer) => {
+    transformer.addProvider('@stouder-io/adonis-geolite2/geolite2_provider')
+  })
 }
